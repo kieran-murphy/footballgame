d52af67b6d798c30eea1a5e1582b7100aba85c06
Kieran <kmurp62rulz@gmail.com>
2020-12-12 12:07:12 +1000

    added VAR

--- game.py
+++ game.py
@@ -15,10 +15,11 @@ class Team:
         self.red = False
 
 
-team1 = Team('Hit or Miss', 0, 50, 50, 50, 50, 50)
-team2 = Team('Yeah the Mooys', 0, 10, 10, 10, 10, 10)
+team1 = Team('Team1', 0, 50, 50, 50, 50, 50)
+team2 = Team('Team2', 0, 30, 20, 20, 20, 20)
 
 match = [team1, team2]
+
 
 def goal(team):
     print("Goal! " + str(team.name) + " scores! ⚽")
@@ -38,16 +39,48 @@ def matchday(match):
     for i in range(0, 91):
         n1 = random.randint(1, 300)
         n2 = random.randint(0,1)
+        
         event = ""
-        if n1 < 282:
+        #nothing happening
+        if n1 < 283:
             if match[0].red == True or match[1].red == True:
                 event = " "
             else:
                 event = " "
-        elif n1 < 294:
-            goal(match[n2])
+        #VAR
+        elif n1 < 284:
+            print("VAR Decision: Checking Possible Foul! 📺")
+            time.sleep(3)
+            nVar = random.randint(0,10)
+            nVar_1 = random.randint(1,50)
+            nVar_2 = random.randint(1,50)
+            if (match[0].luck + nVar_1) > (match[1].luck + nVar_2):
+                varteam = match[0]
+            else:
+                varteam = match[1]
+            if nVar > 3:
+                print("Penalty given for " + str(varteam.name) + "!")
+                nVar_3 = random.randint(0,4)
+                if nVar_3 > 1:
+                    goal(varteam)
+                else:
+                    print("Penalty Missed! ❌")
+            else:
+                print("No Penalty Given! ❌")
+
+        #goal
+        elif n1 < 295:
+            #generate new random number here for each team (plus stats) to compare
+            n3_1 = random.randint(1,50)
+            n3_2 = random.randint(1,50)
+            if (match[0].attack + n3_1) > (match[1].attack + n3_2):
+                goal(match[0])
+            else:
+                goal(match[1])
+        #yellow card
         elif n1 < 299:
             yellow(match[n2])
+        #red card 
         else:
             red(match[n2])
         time.sleep(0.2)
